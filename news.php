66dbd374b9b133afba1a8d7ab611359b38f3adf7
Daria Kozina <kozina.flowers@yandex.ru>
2021-07-27 16:27:39 +0300

    html on pages, render article per page from mysql

--- news.php
+++ news.php
@@ -1,3 +1,46 @@
+<?php 
+    // // get json
+    $json = file_get_contents('php://input');
+    // // data is object
+    $data = json_decode($json);
+    // кол-во новостей на странице
+    // $per_page = 5;
+    
+    //Устанавливаем доступы к базе данных:
+    $host = '127.0.0.1';
+    //  $host = 'localhost'; //имя хоста, на локальном компьютере это localhost
+    $user = 'root'; //имя пользователя, по умолчанию это root
+    $password = 'root'; //пароль, по умолчанию пустой
+    $db_name = 'news'; //имя базы данных
+
+    // для хостинга спринтхост
+    // $host = 'localhost'; 
+    // $user = 'f0497458_root'; 
+    // $password = 'root'; 
+    // $db_name = 'f0497458_avengers'; 
+
+    //Соединяемся с базой данных используя наши доступы:
+    $mysqli = new mysqli($host, $user, $password, $db_name);
+
+    $mysqli->query("CREATE TABLE IF NOT EXISTS number(
+        id INT,
+        num INT,
+        PRIMARY KEY (id)
+        )");
+    $mysqli->query("INSERT INTO number(id, num) VALUES (1, 5)");
+
+    
+    if ($data !== null) {  
+        echo $data->number;
+        $num = $data->number;
+        $mysqli->query("UPDATE number SET id = 1, num = '$num'");
+    }
+    $get_num = $mysqli->query("SELECT num FROM number");
+    $row_number = $get_num->fetch_row();
+    $per_page = $row_number[0];
+    $page = 1;
+    $art = ($page * $per_page) - $per_page;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +48,80 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Новости</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <header class='header'>
+        <h1 class='primary-title'>Новости</h1>
+    </header>
+    <main class='main'>
+        <?php
+            $result = $mysqli->query("SELECT *  FROM news LIMIT $art,$per_page");
+            $row = $result->fetch_assoc();
+            foreach ($result as $row) {
+                echo "
+                <article class='article'>
+                    <span class='date'>" . $row['idate'] . "</span>
+                    <a href='#' class='title'>" . $row['title'] . "</a>
+                    <p class='announce'>" . $row['announce'] . "</p>
+                </article>";
+            }
+        ?>
+    </main>
+    <footer class='footer'>
+        <h3 class='title-3'>Страницы :</h3>
+        <div class='pages-btns'>
+            <?php             
+                if (isset($_GET['page'])){
+                    $page = $_GET['page'];
+                } else $page = 1;
+
+                // кол-во строк в таблице
+                $res = $mysqli->query("SELECT COUNT(*) FROM news");
+                $row2 = $res->fetch_row();
+                $total = $row2[0];
+                // кол-во страниц
+                $pages_amount = ceil($total / $per_page);
+                for ($i = 1; $i <= $pages_amount; $i++) {
+                    echo "<button class='btn'>" . $i . "</button>";
+                    // echo "<a href=lessons.php?page=".$i."> Страница ".$i." </a>";
+                }
+            ?>
+        </div>
+    </footer>
 </body>
+<script>
+    const m = document.querySelector('main'),
+        article = document.querySelector('article'),
+        perPage = Math.floor(m.clientHeight / article.clientHeight);
+    const sendPageInfo = () => {
+        const showError = error => {
+                console.error(error);
+            };
+
+        const postData = body =>  fetch('./news.php', {
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
+
+        window.addEventListener('load', () => {
+            const body = {
+                'number': perPage,
+            };
+            console.log(body);
+
+            postData(body)
+                .then(response => {
+                    if (response.status !== 200) {
+                        throw new Error('network status is not 200');
+                    }
+                })
+                .catch(showError);
+        });
+    };
+    sendPageInfo();
+</script>
 </html>
--- view.php
+++ view.php
@@ -5,8 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Статья</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <header class='header'>
+        <h1 class='primary-title'>Название новости</h1>
+    </header>
+    <main>
+        <p class='content'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Eveniet cupiditate illum aliquam dignissimos sed assumenda fugit eius voluptatibus
+            necessitatibus sapiente excepturi quisquam alias, tenetur, modi iure vitae dolores nobis 
+            distinctio.
+        </p>
+    </main>
+    <footer class='footer'>
+        <a href='news.php'>Все новости > ></a>
+    </footer>
 </body>
 </html>
